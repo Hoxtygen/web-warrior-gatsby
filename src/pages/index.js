8da0be9f3b36b92783668abc5cd8604a0f0362a4
Wasiu Idowu <hoxtygen@live.com>
2021-09-22 22:40:02 +0100

    Make graphql queries inside pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,41 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
-import { Link } from 'gatsby';
+import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home({data}) {
+  console.log("data:", data)
+  const {title, description} = data.site.siteMetadata
   return (
     <Layout>
-      <section className = {styles.header}>
+      <section className={styles.header}>
         <div>
-          <h2 >Design</h2>
+          <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web deveoper based in Manchester</p>
-          <Link to = "/projects" className = {styles.btn}>My Portfolio Projects</Link>
-          <img src="/banner.png" alt="site banner" style = {{maxWidth: "100%"}} />
-        </div> 
+          <Link to="/projects" className={styles.btn}>
+            My Portfolio Projects
+          </Link>
+          <img
+            src="/banner.png"
+            alt="site banner"
+            style={{ maxWidth: "100%" }}
+          />
+          <p>{title} -- {description}</p>
+        </div>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query siteInfo {
+    site(siteMetadata: {}) {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
